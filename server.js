5181061e60c91a6ec6ac0c9b90be763f0aaa5820
Izzy Park <isabelle.y.park@gmail.com>
2015-09-28 07:45:55 -0700

    combined moackaroo and server functionality. should refactor later.

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ var http = require('http'),
 	bodyParser = require('body-parser'), //need this to parse body request
 	express = require('express'),
 	app = express(),
-    faye = require('faye');
+    faye = require('faye'),
+    Mockaroo = require('mockaroo');
 
+var client = new Mockaroo.Client({
+    apiKey: '54f347b0'
+})
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
 app.post('/message', function(req, res) {
-  bayeux.getClient().publish('/channel', {text: req.body.message});
+
+  client.generate({
+	    count: 2,
+	    schema: 'Emerson Sensi Feed Data'
+	}).then(function(records) {
+	    //process the records
+	    for (var i=0; i<records.length; i++) {
+	        var record = records[i];
+	        //console.log(record);
+	        bayeux.getClient().publish('/channel', {text: record.indoor_equipment});
+	    }
+  	});
+
   res.send(200);
 });
 
@@ -19,3 +35,8 @@ var server = http.createServer(app),
 bayeux.attach(server);
 server.listen(8000);
 console.log('server listening to 8000')
+
+
+
+
+
