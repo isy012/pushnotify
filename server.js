04605265116701aadc07c7ba3fb7eaa005c62325
Izzy Park <isy012@hotmail.com>
2015-09-27 17:06:05 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+	bodyParser = require('body-parser'),
+	express = require('express'),
+	app = express(),
+    faye = require('faye');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+app.post('/message', function(req, res) {
+  console.log(req.body.message);
+  bayeux.getClient().publish('/channel', {text: req.body.message});
+  res.send(200);
+});
+
+var server = http.createServer(app),
+    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+bayeux.attach(server);
+server.listen(8000);
+console.log('server listening to 8000')
